84cf33f208eb78d3ba028174b118b0c63a8405f8
Winny Didine Ineza <116871728+winnyineza@users.noreply.github.com>
2024-06-04 11:22:41 +0200

    Done

--- 0x08-making_change /0-making_change.py
+++ 0x08-making_change /0-making_change.py
@@ -1,25 +1,10 @@
 #!/usr/bin/python3
-"""Change making module.
+"""
+Main file for testing
 """
 
+makeChange = __import__('0-making_change').makeChange
 
-def makeChange(coins, total):
-    """Determines the fewest number of coins needed to meet a given
-    amount total when given a pile of coins of different values.
-    """
-    if total <= 0:
-        return 0
-    rem = total
-    coins_count = 0
-    coin_idx = 0
-    sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while rem > 0:
-        if coin_idx >= n:
-            return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
-            coins_count += 1
-        else:
-            coin_idx += 1
-    return coins_count
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
